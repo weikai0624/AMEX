308c0a27683fd147a4e0db2799a6d04f54a38b16
Weikai <weikai860624@gmail.com>
2022-12-01 17:16:55 +0800

    Add, discount data models.

--- map/admin.py
+++ map/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.apps import apps
+# Register your models here.
+
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
