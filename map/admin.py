b79b863bdfc24b654f5a1400e0e8762a9862f389
Weikai <weikai860624@gmail.com>
2022-12-06 17:06:08 +0800

    Refactory, models admin list_display and blank.

--- map/admin.py
+++ map/admin.py
@@ -3,7 +3,7 @@ from map.models import DiscountData
 
 
 class DiscountDataAdmin(admin.ModelAdmin):
-    list_display = ('place', 'card_name','card', 'discount_type', 'address', 'google_map_url', 'udpate_datetime')
+    list_display = ('place', 'card_name','card', 'discount_type', 'address', 'google_map_url', 'udpate_datetime', 'longitude', 'latitude')
     list_filter = ('card', 'card_name', 'place', 'udpate_datetime')
 
 admin.site.register(DiscountData, DiscountDataAdmin)
